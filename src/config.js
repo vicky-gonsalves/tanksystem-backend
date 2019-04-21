a82659d569fcb305660ffde3035de10a20c115e9
vicky <vgonsalves@arcfix.com>
2019-04-21 13:35:36 +0530

    Initial

--- src/config.js
+++ src/config.js
@@ -54,7 +54,10 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/tanksystem'
+      uri: process.env.MONGODB_URI || 'mongodb://localhost/tanksystem',
+      options: {
+        debug: true
+      }
     }
   }
 }
