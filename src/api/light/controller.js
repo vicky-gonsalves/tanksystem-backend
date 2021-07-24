fecb8e65288b424a9e7bcb1781ae208c48549f90
Vicky Gonsalves <vicky.gonsalves@outlook.com>
2021-07-24 12:46:29 +0530

    added state route

--- src/api/light/controller.js
+++ src/api/light/controller.js
@@ -29,7 +29,7 @@ export const state = (res, next) =>
   Light.findOne({$or: [{light1: true}, {light2: true}, {light3: true}, {light4: true}]})
     .then((light) => light ? 'ON' : 'OFF')
     .then((state)=>{
-      res.status(200).send(state);
+      return res.status(200).send(state);
     })
     .catch(next);
 
