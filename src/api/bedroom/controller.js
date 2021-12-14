c18ea3dbe8a299ebfc82029bd9b51a70d0fc16cb
Vicky Gonsalves <vicky.gonsalves@truepill.com>
2021-12-15 00:38:55 +0530

    fix: corrected update api

--- src/api/bedroom/controller.js
+++ src/api/bedroom/controller.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {Bedroom} from '.'
 import {notFound, success} from '../../services/response/'
 import * as wol from 'wol';
@@ -31,31 +32,25 @@ export const update = ({bodymen: {body}, params}, res, next) =>
     .then(notFound(res))
     .then((getStatus) => {
       if (getStatus) {
-        if (body.hasOwnProperty('light1')) {
-          getStatus.markModified('light1');
-          getStatus.light1 = body.light1;
-        }
-        if (body.hasOwnProperty('light2')) {
-          getStatus.markModified('light2');
-          getStatus.light2 = body.light2;
-        }
-        if (body.hasOwnProperty('light3')) {
-          getStatus.markModified('light3');
-          getStatus.light3 = body.light3;
-        }
-        if (body.hasOwnProperty('fan')) {
-          getStatus.markModified('fan');
-          getStatus.fan = body.fan;
-        }
-        if (body.hasOwnProperty('websocket')) {
-          getStatus.markModified('websocket');
-          getStatus.websocket = body.websocket;
-        }
-        if (body.hasOwnProperty('updatedByDevice')) {
-          getStatus.markModified('updatedByDevice');
-          getStatus.updatedByDevice = body.updatedByDevice;
-        }
-        return getStatus.save();
+        // if (body.hasOwnProperty('light1')) {
+        //   getStatus.markModified('light1');
+        // }
+        // if (body.hasOwnProperty('light2')) {
+        //   getStatus.markModified('light2');
+        // }
+        // if (body.hasOwnProperty('light3')) {
+        //   getStatus.markModified('light3');
+        // }
+        // if (body.hasOwnProperty('fan')) {
+        //   getStatus.markModified('fan');
+        // }
+        // if (body.hasOwnProperty('websocket')) {
+        //   getStatus.markModified('websocket');
+        // }
+        // if (body.hasOwnProperty('updatedByDevice')) {
+        //   getStatus.markModified('updatedByDevice');
+        // }
+        return Object.assign(getStatus, body).save();
       }
     })
     .then((getStatus) => getStatus ? getStatus.view(true) : null)
